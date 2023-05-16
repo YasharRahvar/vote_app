b1a918740c2ba0c9655b1d064a24ae8e356f7ac4
Yashar <yashar.rahvar@hmcts.net>
2023-05-16 13:18:20 +0100

    updating file

--- src/main/java/com/demo/voting/model/PlayPackage.java
+++ src/main/java/com/demo/voting/model/PlayPackage.java
@@ -1,4 +1,18 @@
 package com.demo.voting.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
 public class PlayPackage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int PlayPackageId;
+
+    @Column
+    private Strategy strategy;
+
 }
